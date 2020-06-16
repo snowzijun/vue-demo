98811bee6212373c215b9a5799965489586f99f4
fengc-c <fengc-c@fengc-cdeMacBook-Pro.local>
2020-06-16 22:49:31 +0800

    完善示例

---
+++ src/components/loading/index.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-16 22:34:00
+ * @LastEditTime: 2020-06-16 22:44:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue-test/src/components/loading/index.js
+ */
+import Vue from 'vue'
+import LoadingComponent from './loading.vue'
+
+// 通过Vue.extend将组件包装成一个子类
+const LoadingConstructor = Vue.extend(LoadingComponent)
+
+let loading = undefined
+
+LoadingConstructor.prototype.close = () => {
+  // 如果loading 有引用，则去掉引用
+  if (loading) {
+    loading = undefined
+  }
+  // 先将组件隐藏
+  this.visible = false
+  // 延迟300毫秒，等待loading关闭动画执行完之后销毁组件
+  setTimeout(() => {
+    // 移除挂载的dom元素
+    if (this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el)
+    }
+    // 调用组件的$destroy方法进行组件销毁
+    this.$destroy()
+  }, 300)
+}
